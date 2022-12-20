121448f00d4f69dc51a2fe1ae9e90881e0d44c77
alondramora <mora_ac@yahoo.com>
2022-12-20 13:46:57 -0800

    added pop up modal

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// gift open
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close-button');
+
+open.addEventListener('click', () => {
+    modal_container.classList.add('show')
+});
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show')
+})
